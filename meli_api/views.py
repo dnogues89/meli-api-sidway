5cb19cac466b7df47961cfa33ed218b43325475f
dnogues89 <damiannogues@gmail.com>
2024-05-16 14:48:00 -0300

    arreglando los stats

--- meli_api/views.py
+++ meli_api/views.py
@@ -11,27 +11,30 @@ def mis_pubs(request):
 
 def update_stats(request):
     pubs = models.Publicacion.objects.all()
-    api = MeliAPI(models.MeliCon.objects.get(name = 'API Dnogues'))
+    api = MeliAPI(models.MeliCon.objects.get(name='API Dnogues'))
     hasta = timezone.now().date().strftime('%Y-%m-%dT00:00:00.000')
     lista_pubs = [x.pub_id for x in pubs]
-    resp = api.phone_by_items(','.join(lista_pubs),hasta)
     
-    #Contactos telefonicos
-    if models.resp_ok(resp, 'Obtener clicks en telefonos'):
-        for item in resp.json():
-            try:
-                pub = pubs.get(pub_id = item['item_id'])
-                pub.stats.clics_tel = item['total']
-                pub.stats.save()
-            except:
-                pass
+    # Dividir la lista de publicaciones en sublistas de 5 elementos
+    sublistas_pubs = [lista_pubs[i:i+5] for i in range(0, len(lista_pubs), 5)]
+    
+    for sublist in sublistas_pubs:
+        resp = api.phone_by_items(','.join(sublist), hasta)
+        
+        # Contactos telefónicos
+        if models.resp_ok(resp, 'Obtener clicks en telefonos'):
+            for item in resp.json():
+                try:
+                    pub = pubs.get(pub_id=item['item_id'])
+                    pub.stats.clics_tel = item['total']
+                    pub.stats.save()
+                except:
+                    pass
     
     #Vistas
     for item in pubs.exclude(stats__isnull=True):
-        print(item.pub_id)
         desde = item.f_creado.strftime('%Y-%m-%dT00:00:00.000-00:00')
         resp = api.views_by_item(item.pub_id,desde,hasta)
-        print(resp, resp.status_code)
         if models.resp_ok(resp, f'Get {item.pub_id} views'):
             print(resp.json()[0]['total_visits'])
             item.stats.views = int(resp.json()[0]['total_visits'])
@@ -68,7 +71,14 @@ def sincro_meli(request):
                         stats = models.PubStats.objects.create(pub_id = resp['id']).save()
                         modelo = models.Modelo.objects.get(descripcion__iexact = resp['title'])
                         models.Publicacion.objects.create(pub_id = resp['id'], titulo = resp['title'],desc = resp['descriptions'],precio=resp['price'],categoria = resp['listing_type_id'],activa = activa, url = resp['permalink'],sincronizado = True, modelo = modelo, stats=stats).save()
+
                     except:
                         stats = models.PubStats.objects.create(pub_id = resp['id'])
                         models.Publicacion.objects.create(pub_id = resp['id'], titulo = resp['title'],desc = resp['descriptions'],precio=resp['price'],categoria = resp['listing_type_id'],activa = activa, url = resp['permalink'],sincronizado = True, stats=stats).save()
+
+                    item = models.Publicacion.objects.get(pub_id = resp['id'])
+                    stats = models.PubStats.objects.get(pub_id = resp['id'])
+                    item.f_creado = resp['date_created']
+                    item.stats = stats
+                    item.save()
     return HttpResponse('Archivo no encontrado.')
