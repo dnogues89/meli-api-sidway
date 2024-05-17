c2c4db790bf2ebd77ee699743d5634b7c1d744d3
dnogues89 <damiannogues@gmail.com>
2024-05-17 13:59:57 -0300

    schedule

---
+++ schedul_requests.py
@@ -0,0 +1,57 @@
+import requests
+import schedule
+from schedule import every,repeat
+import time
+
+
+@repeat(every().day.at('9:00'))
+@repeat(every().day.at('12:00'))
+@repeat(every().day.at('17:00'))
+def update_espasa_db():
+    try:
+        response = requests.get('http://meli.dnoguesdev.com.ar/espasa/update_db/')
+        if response.status_code == 200:
+            print(response.text)
+    except:
+        print("No se pudo conectar con Espasa db")
+        print()
+
+        
+@repeat(every(5).minutes)
+def answer_questions():
+    try:
+        response = requests.get('http://meli.dnoguesdev.com.ar/api/preguntas/')
+        if response.status_code == 200:
+            print(response.text)
+    except:
+        print("No se pudo responder preguntas")
+        print()
+        
+@repeat(every(5).minutes)
+def get_leads():
+    try:
+        response = requests.get('http://meli.dnoguesdev.com.ar/leads/get_leads/')
+        if response.status_code == 200:
+            print(response.text)
+    except:
+        print("No se pudo sincrinizar leads")
+        print()
+
+
+@repeat(every(5).minutes)
+def answer_questions():
+    try:
+        response = requests.get('http://meli.dnoguesdev.com.ar/api/get_stats/')
+        if response.status_code == 200:
+            print(response.text)
+    except:
+        print("No se pudo conseguir los stats")
+        print()
+
+
+update_espasa_db()
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
