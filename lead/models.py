a2b8e26a59cffedc063c35e31ce38f90f6c74a98
dnogues89 <damiannogues@gmail.com>
2024-05-24 11:24:35 -0300

    Agrego el campo origen al lead, asi no duplico los q se comunicaron por whatsapp

--- lead/models.py
+++ lead/models.py
@@ -6,6 +6,7 @@ class Lead(models.Model):
     lead_id = models.CharField(max_length=100)
     item_id = models.ForeignKey(Publicacion, null=True, on_delete=models.SET_NULL)
     modelo = models.CharField(max_length=100)
+    origen = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
