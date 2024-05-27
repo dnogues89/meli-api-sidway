cff1a17a5a0d8aa2dcc55378b8baadd3daa7dcfe
dnogues89 <damiannogues@gmail.com>
2024-05-27 16:00:28 -0300

    aarreglando el nivel en el compose migrations

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "inicio de build"
+
+echo "Cambio directorio app"
+cd /opt/Meli_api_autos/images
+
+echo "Mato docker instance"
+docker compose down
+cd ..
+
+echo "Pull de Github"
+git pull
+
+echo "Build instancia docker"
+docker build -t meli-api-autos:v1 .
+cd images
+
+echo "Inicio instancia docker"
+docker compose up -d
