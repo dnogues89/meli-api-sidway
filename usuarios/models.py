0d846d5b2bf171e16afe788d8177077f673607d3
dnogues89 <damiannogues@gmail.com>
2024-05-27 11:08:43 -0300

    usuarios cuentas y secretos

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Cuenta(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    code_tg = models.CharField(max_length=300)
+    token = models.CharField(max_length=300)
+    refresh_secret = models.CharField(max_length=300)
     meli_id = models.CharField(max_length=10, null=True)
     
     class Meta:
