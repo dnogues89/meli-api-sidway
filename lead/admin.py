0e47a9accb56f92b356b8d421f3ff7dde5011358
dnogues89 <damiannogues@gmail.com>
2024-05-27 17:35:38 -0300

    estadisticas

--- lead/admin.py
+++ lead/admin.py
@@ -1,5 +1,7 @@
+
 from django.contrib import admin
-from .models import Lead
+
+from .models import Lead, Estadisticas
 
 # Register your models here.
 @admin.register(Lead)
@@ -9,3 +11,19 @@ class LeadAdmin(admin.ModelAdmin):
     ordering = ['-date']
     date_hierarchy = 'date'
     list_per_page = 500
+
+@admin.register(Estadisticas)
+class EstadisticasAdmin(admin.ModelAdmin):
+    list_display = [
+        'cuenta',
+        "hoy",
+        "ayer",
+        "ultimos_3_dias",
+        "x_dia_30_dias",
+        "total",
+    ]
+    
+    def get_queryset(self, request):
+        estadisticas, created = Estadisticas.objects.get_or_create(pk=1)
+        estadisticas.actualizar_estadisticas()
+        return super().get_queryset(request).filter(pk=1)
--- lead/models.py
+++ lead/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from meli_api.models import Publicacion
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
 class Lead(models.Model):
@@ -14,3 +15,28 @@ class Lead(models.Model):
     date = models.DateTimeField(null=True, blank=True)
     to_crm = models.BooleanField(default=False)
     contactos = models.IntegerField(default=0)
+    
+class Estadisticas(models.Model):
+    cuenta = models.CharField(default='Estadisticas LIMA')
+    hoy = models.IntegerField(default=0)
+    ayer = models.IntegerField(default=0)
+    ultimos_3_dias = models.IntegerField(default=0)
+    x_dia_30_dias = models.FloatField(default=0.0)
+    total = models.IntegerField(default=0)
+
+    def actualizar_estadisticas(self):
+        ahora = timezone.now()
+        hoy_inicio = ahora.replace(hour=0, minute=0, second=0, microsecond=0)
+        ayer_inicio = hoy_inicio - timedelta(days=1)
+        tres_dias_inicio = hoy_inicio - timedelta(days=3)
+        treinta_dias_inicio = hoy_inicio - timedelta(days=30)
+
+        self.hoy = Lead.objects.filter(date__gte=hoy_inicio).count()
+        self.ayer = Lead.objects.filter(date__gte=ayer_inicio, date__lt=hoy_inicio).count()
+        self.ultimos_3_dias = Lead.objects.filter(date__gte=tres_dias_inicio).count()
+        self.x_dia_30_dias = int(Lead.objects.filter(date__gte=treinta_dias_inicio).count() / 30)
+        self.total = Lead.objects.all().count()
+        self.save()
+
+    def __str__(self):
+        return "Estadisticas"
