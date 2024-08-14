e3ad224fcff3e2e6452c5e464998441e6fac19c4
dnogues89 <damiannogues@gmail.com>
2024-08-14 13:13:43 -0300

    asd

--- multiple_upload/urls.py
+++ multiple_upload/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('upload', views.fileupload, name = "File_Uploads"),
+    path('upload/', views.fileupload, name = "File_Uploads"),
     path('confirmacion/', views.confirmacion, name='Confirmacion')
 ]
