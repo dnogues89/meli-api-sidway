fd4b017577b324163c18a2da4ffeec478ad85309
dnogues89 <damiannogues@gmail.com>
2024-10-28 12:50:29 -0300

    asd

--- lead/admin.py
+++ lead/admin.py
@@ -9,31 +9,31 @@ from datetime import datetime
 
 # Register your models here.
 
-@admin.register(Cuit)
-class CuitAdmin(ModelAdmin):
-    list_display = ['nombre','localidad','dni','cuil','usados']
-    search_fields = ['nombre','dni','cuil']
-    actions = ('siomaa_api',)
+# @admin.register(Cuit)
+# class CuitAdmin(ModelAdmin):
+#     list_display = ['nombre','localidad','dni','cuil','usados']
+#     search_fields = ['nombre','dni','cuil']
+#     actions = ('siomaa_api',)
 
-    @admin.action(description="Api SIOMAA")
-    def siomaa_api(self,request,objetos):
-        for obj in objetos:
-            siomaa = Sioma_API(obj.cuit).get_data()
-            if siomaa:
-                for item in siomaa['HistoricoCompras']:
-                    usado = Usado.objects.create(
-                        compra=datetime.strptime(item['FechaOperacion'], '%Y-%m-%dT%H:%M:%S').date() if item['FechaOperacion'] else None,
-                        marca=item['Marca'],
-                        modelo=item['Modelo'],
-                        version=item['Version'],
-                        anio=item['AnioModelo'],
-                        cerokm=True if item['C0KM'] == 'Si' else False,
-                        venta=datetime.strptime(item['FechaVenta'], '%Y-%m-%dT%H:%M:%S').date() if item['FechaVenta'] else None,
-                        tipo_compra='Prenda' if item['TipoCompra'] == 'Prenda' else 'Cash',
-                        tipo_acreedor=item['TipoAcreedor'],
-                        acreedor=item['Acreedor']
-                    ).save()
-                    obj.usados.add(usado)
+#     @admin.action(description="Api SIOMAA")
+#     def siomaa_api(self,request,objetos):
+#         for obj in objetos:
+#             siomaa = Sioma_API(obj.cuit).get_data()
+#             if siomaa:
+#                 for item in siomaa['HistoricoCompras']:
+#                     usado = Usado.objects.create(
+#                         compra=datetime.strptime(item['FechaOperacion'], '%Y-%m-%dT%H:%M:%S').date() if item['FechaOperacion'] else None,
+#                         marca=item['Marca'],
+#                         modelo=item['Modelo'],
+#                         version=item['Version'],
+#                         anio=item['AnioModelo'],
+#                         cerokm=True if item['C0KM'] == 'Si' else False,
+#                         venta=datetime.strptime(item['FechaVenta'], '%Y-%m-%dT%H:%M:%S').date() if item['FechaVenta'] else None,
+#                         tipo_compra='Prenda' if item['TipoCompra'] == 'Prenda' else 'Cash',
+#                         tipo_acreedor=item['TipoAcreedor'],
+#                         acreedor=item['Acreedor']
+#                     ).save()
+#                     obj.usados.add(usado)
 
 
 @admin.register(CuitInfo)
